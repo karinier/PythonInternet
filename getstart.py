d3fdcd60e89242d86079083e95ca97b636ea5547
d3k4r <d3k4r@users.noreply.github.com>
2016-10-27 00:25:25 +0300

    Remove test files

--- getstart.py
+++
@@ -1,8 +0,0 @@
-import flask, flask.view
-app=flask.Flask(__name__)
-class views(flask.view.MethodView):
-    def get(self):
-        return "Hello World!"
-    app.add_url_rule ( '/ ', view_func=view.asview('main'))
-app.debug=TRUE
-app.run()
