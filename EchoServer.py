33b30e3488efb57640dbc32069d1617d992689f8
Robert Kariniemi <robert.kariniemi@gmail.com>
2016-10-27 14:22:01 -0500

    deleted comments

--- EchoServer.py
+++ EchoServer.py
@@ -13,9 +13,7 @@ METHOD_NOT_ALLOWED = 'HTTP/1.1 405 Method Not Allowed{}{}{}'.format(CRLF,CRLF,CR
 
 
 def handle_request(request):
-    # here the request can be handled
-    # right now it just takes the bytes that are received and
-    # decodes them as utf-8 bytes and prints it out
+    
 
     print(request.decode('utf-8'))
     lines = request.splitlines()
@@ -37,26 +35,14 @@ def handle_request(request):
 
 def client_talk(client_sock, client_addr):
     print('talking to {}'.format(client_addr))
-    # it looks like it just receives connections
-    # and then prints out what it received then
-
-    # for more data to receive and prints it out
-    # keeps looping doing that.
+    
     data = client_sock.recv(BUFSIZE)
     while data:
-        # here is where we need to handle the GET/HEAD requests
-        # and everything basically.
-        # a good way to do this is to make a function here, and pass
-        # the data to it so that you can keep the logic separate.
+        
         handle_request(data)
         data = client_sock.recv(BUFSIZE)
 
-    # if the client (the remote side) disconnects then it falls to here
-    # and this thread is cleaned up. This can be seen by closing the browser
-    # tab or stopping the browser from loading the localhost:9001 page.
-    # or by ctrl-c killing the curl command
-
-    # clean up
+    
     client_sock.shutdown(1)
     client_sock.close()
     print('connection closed.')
@@ -65,20 +51,14 @@ def client_talk(client_sock, client_addr):
 class EchoServer:
     def __init__(self, host, port):
         print('listening on port {}'.format(port))
-        # the command line arguments host and port are assigned locally here
-        # we have an Object/Class that is called EchoServer and we attach
-        # this host and port to it, e.g. self.host = host will assign host to
-        # this instance of the EchoServer class
+        
         self.host = host
         self.port = port
 
-        # okay, this is where the binding of the socket is done.
-        # so that when we make connections to this host on this port
-        # it will come to this socket
+        
         self.setup_socket()
 
-        # this is a loop to accept connections on that socket that we
-        # just opened in setup_socket
+        
         self.accept()
 
         self.sock.shutdown()
@@ -86,7 +66,7 @@ class EchoServer:
 
     def setup_socket(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # it's bound to localhost and port 9001 by default
+        
         self.sock.bind((self.host, self.port))
         self.sock.listen(128)
 
@@ -96,18 +76,16 @@ class EchoServer:
         while True:
             (client, address) = self.sock.accept()
 
-            # it creates this thread then starts it and then loops back to waiting
-            # for more connections while the Thread runs elsewhere.
-            # client_talk is the handler function
+           
             th = Thread(target=client_talk, args=(client, address))
             th.start()
 
 
 def parse_args():
-    # this is the parsing function
+    
     parser = ArgumentParser()
 
-    # default is localhost if no argument is given
+    
     parser.add_argument('--host', type=str, default='localhost',
                         help='specify a host to operate on (default: localhost)')
     parser.add_argument('-p', '--port', type=int, default=9001,
@@ -117,12 +95,10 @@ def parse_args():
 
 
 if __name__ == '__main__':
-    # program starts here
-    # parses the command line arguments -host and -port into those
-    # variables host and port, if they are found.
+   
     (host, port) = parse_args()
 
-    # this is where the actual server part starts, in this function
+    
     EchoServer(host, port)
 
 
